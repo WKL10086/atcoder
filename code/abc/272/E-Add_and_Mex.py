47ff5072f1ed1133cc89b7c82588ed4b82dc7916
WKL10086 <w64935656@gmail.com>
2022-10-16 02:28:32 +0800

    TLE

--- code/abc/272/E-Add_and_Mex.py
+++ code/abc/272/E-Add_and_Mex.py
@@ -4,26 +4,24 @@ M = int(M)
 A = input().split()
 A = list(map(int, A))
 
-mex = 1
+mex = 0
 
 counter = 0
 while counter < M:
-    if mex == 0:
-        print(0)
-    else:
-        i = 0
-        while i < N:
-            A[i] = A[i] + i + 1
-            i += 1
+    i = 0
+    while i < N:
+        A[i] = A[i] + i + 1
+        i += 1
 
-        filterd = [ele for ele in A if ele >= 0]
-        filterd.sort()
+    filtered = [ele for ele in A if ele >= 0]
+    filtered.sort()
 
-        j = 0
-        while j < N:
-            if j not in filterd:
-                mex = j
-            j += 1
+    j = 0
+    while j < N:
+        if j not in filtered:
+            mex = j
+            break
+        j += 1
 
     print(mex)
     counter += 1
