eed03d28fcdb39397645af8e2c1211d394d70cdb
WKL10086 <w64935656@gmail.com>
2022-10-27 01:37:17 +0800

    Add 134

--- atcoder/abc/134/C-Exception_Handling.py
+++ atcoder/abc/134/C-Exception_Handling.py
@@ -1,15 +1,30 @@
 N = int(input())
+A = [0 for _ in range(N)]
 
-max_1st = 0
-max_2nd = 0
+biggest = 0
+biggest_2nd = 0
+
 for i in range(N):
-    A = int(input())
+    A[i] = int(input())
 
-    if A > max_1st:
-        max_2nd = max_1st
-        max_1st = A
-
-    if A == max_1st:
-        print(max_2nd)
+    if i == 0:
+        continue
+    elif i == 1:
+        if A[0] > A[1]:
+            biggest = A[0]
+            biggest_2nd = A[1]
+        else:
+            biggest_2nd = A[0]
+            biggest = A[1]
     else:
-        print(max_1st)
+        if A[i] >= biggest:
+            biggest_2nd = biggest
+            biggest = A[i]
+        elif A[i] >= biggest_2nd:
+            biggest_2nd = A[i]
+
+for i in range(N):
+    if A[i] == biggest:
+        print(biggest_2nd)
+    else:
+        print(biggest)
