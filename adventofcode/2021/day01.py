f14b2454232b0a391eaa606b5230a6e9f68fe992
Peter Wong <peter.wong@deepcodify.com>
2022-12-01 15:23:56 +0800

    Add template

--- adventofcode/2021/day01.py
+++ adventofcode/2021/day01.py
@@ -1,49 +1,50 @@
-def part1():
-    with open("day01.txt", "r") as f:
-        data = f.read().splitlines()
+def part1(data: list[str]):
+    N = len(data)
 
-        N = len(data)
+    prev = 0
+    counter = 0
+    for i in range(N):
+        current = int(data[i])
 
-        prev = 0
-        counter = 0
-        for i in range(N):
-            current = int(data[i])
-
-            if i == 0:
-                prev = current
-                continue
-            else:
-                if current > prev:
-                    counter += 1
-
-                prev = current
-
-        print(counter)
-
-        f.close()
-
-
-def part2():
-    with open("day01.txt", "r") as f:
-        data = f.read().splitlines()
-        data = list(map(int, data))
-
-        N = len(data)
-
-        prev = data[0] + data[1] + data[2]
-        counter = 0
-
-        i = 1
-        while i + 2 < N:
-            current = data[i] + data[i + 1] + data[i + 2]
-
+        if i == 0:
+            prev = current
+            continue
+        else:
             if current > prev:
                 counter += 1
 
             prev = current
 
-            i += 1
+    print(counter)
 
-        print(counter)
 
-        f.close()
+def part2(data: list[str]):
+    data = list(map(int, data))
+
+    N = len(data)
+
+    prev = data[0] + data[1] + data[2]
+    counter = 0
+
+    i = 1
+    while i + 2 < N:
+        current = data[i] + data[i + 1] + data[i + 2]
+
+        if current > prev:
+            counter += 1
+
+        prev = current
+
+        i += 1
+
+    print(counter)
+
+
+with open("day01.txt", "r") as f:
+    data = f.read().splitlines()
+
+    part1(data)
+
+    part2(data)
+
+    f.close()
--- adventofcode/2021/day02.py
+++ adventofcode/2021/day02.py
@@ -1,46 +1,46 @@
-def part1():
-    with open("day02.txt", "r") as f:
-        data = f.read().splitlines()
+def part1(data: list[str]):
+    horizontal = 0
+    depth = 0
 
-        horizontal = 0
-        depth = 0
+    for ele in data:
+        command, num = ele.split()
+        num = int(num)
 
-        for ele in data:
-            command, num = ele.split()
-            num = int(num)
+        if command == "forward":
+            horizontal += num
+        elif command == "down":
+            depth += num
+        elif command == "up":
+            depth -= num
 
-            if command == "forward":
-                horizontal += num
-            elif command == "down":
-                depth += num
-            elif command == "up":
-                depth -= num
-
-        print(horizontal * depth)
-
-        f.close()
+    print(horizontal * depth)
 
 
-def part2():
-    with open("day02.txt", "r") as f:
-        data = f.read().splitlines()
+def part2(data: list[str]):
+    horizontal = 0
+    depth = 0
+    aim = 0
 
-        horizontal = 0
-        depth = 0
-        aim = 0
+    for ele in data:
+        command, num = ele.split()
+        num = int(num)
 
-        for ele in data:
-            command, num = ele.split()
-            num = int(num)
+        if command == "forward":
+            horizontal += num
+            depth += aim * num
+        elif command == "down":
+            aim += num
+        elif command == "up":
+            aim -= num
 
-            if command == "forward":
-                horizontal += num
-                depth += aim * num
-            elif command == "down":
-                aim += num
-            elif command == "up":
-                aim -= num
+    print(horizontal * depth)
 
-        print(horizontal * depth)
 
-        f.close()
+with open("day02.txt", "r") as f:
+    data = f.read().splitlines()
+
+    part1(data)
+
+    part2(data)
+
+    f.close()
