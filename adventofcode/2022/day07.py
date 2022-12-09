cb5e750d61f3a49517863c4f1ed73b979a105646
Peter Wong <peter.wong@deepcodify.com>
2022-12-09 15:31:09 +0800

    Finish day07

--- adventofcode/2022/day07.py
+++ adventofcode/2022/day07.py
@@ -1,72 +1,118 @@
+from __future__ import annotations
+
+
 class Tree:
-    def __init__(self, name: str, size: int):
+    def __init__(self, name: str, size: int, parent: Tree = None):
         self.children = []
         self.name = name
         self.size = size
+        self.parent = parent
 
-    def insertFile(self, name: str, data: int):
-        self.children.append(Tree(name, data))
+    def insertFile(self, name: str, size: int):
+        self.children.append(Tree(name, size, self))
         self.size = self.computeSize()
 
     def insertDir(self, name: str):
-        self.children.append(Tree(name, 0))
+        self.children.append(Tree(name, 0, self))
 
-    def computeSize(self):
+    def computeSize(self) -> int:
         if len(self.children) == 0:
             return self.size
         else:
             size = 0
             for child in self.children:
                 size += child.computeSize()
+            self.size = size
             return size
 
+    def navigate(self, path: str):
+        if path == "..":
+            return self.parent
+        else:
+            for child in self.children:
+                if child.name == path:
+                    return child
 
-def handleAction_cd(path: str, currentPath: list[str]):
-    if path == "..":
-        currentPath.pop()
-    else:
-        currentPath.append(path)
+    def findSmallestDir(self, neededSize: int) -> int:
+        if len(self.children) == 0:
+            return 70000000
+        else:
+            ans = self.size
+            for child in self.children:
+                temp = min(ans, child.findSmallestDir(neededSize))
+                if temp >= neededSize:
+                    ans = temp
+
+            return ans
 
 
-def findTree(currentTree: Tree, currentPath: list[str]):
-    if len(currentPath) == 0:
-        return currentTree
-    else:
-        for child in currentTree.children:
-            if child.name == currentPath[0]:
-                return findTree(child, currentPath[1:])
+def findSumOfDirSizeAtMost100000(tree: Tree):
+    ans = 0
+    for child in tree.children:
+        ans += findSumOfDirSizeAtMost100000(child)
+
+    # check if current tree is a directory and its size is at most 100000
+    if tree.size <= 100000 and len(tree.children) > 0:
+        ans += tree.size
+
+    return ans
 
 
-def part1(data: list[str]):
-    currentPath = []
+def findNeededSize(root: Tree):
+    usedSize = root.size
+    return usedSize - 40000000
+
+
+def createTree(data: list[str]):
+    root = None
+    currentTree = None
 
     for line in data:
         command = line.split()
-        firstLetter = command[0][0]
+        firstLetter = command[0]
         if firstLetter == "$":
             action = command[1]
             if action == "cd":
                 path = command[2]
-                handleAction_cd(path, currentPath)
+                if path == "/":
+                    root = Tree("/", 0)
+                    currentTree = root
+                else:
+                    currentTree = currentTree.navigate(path)
             elif action == "ls":
                 continue
         else:
-            size = int(command[0])
-            name = command[1]
+            if command[0] == "dir":
+                currentTree.insertDir(command[1])
+            else:
+                size = int(command[0])
+                name = command[1]
+                currentTree.insertFile(name, size)
+
+    root.computeSize()
+
+    return root
+
+
+def part1(data: list[str]):
+    root = createTree(data)
+
+    print(findSumOfDirSizeAtMost100000(root))
 
 
 def part2(data: list[str]):
-    ans = 0
+    root = createTree(data)
+    neededSize = findNeededSize(root)
 
-    print(ans)
+    print(root.findSmallestDir(neededSize))
 
 
-with open("test.txt", "r") as f:
-    # with open("day07.txt", "r") as f:
+# with open("test.txt", "r") as f:
+with open("day07.txt", "r") as f:
     data = f.read().splitlines()
 
-    part1(data)
+    # part1(data)
 
-    # part2(data)
+    part2(data)
 
     f.close()
