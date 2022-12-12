0ca8c628fe30c713b106c1e948c5ec3336294bc3
Peter Wong <peter.wong@deepcodify.com>
2022-12-12 15:38:23 +0800

    Add test func in template

--- adventofcode/template/day.py
+++ adventofcode/template/day.py
@@ -1,21 +1,32 @@
-def part1(data: list[str]):
+def part1(data: list[str]) -> int:
     ans = 0
 
     print(ans)
+    return ans
 
 
-def part2(data: list[str]):
+def part2(data: list[str]) -> int:
     ans = 0
 
     print(ans)
+    return ans
 
 
-with open("test.txt", "r") as f:
-    # with open("day.txt", "r") as f:
+def test_passing():
+    with open("test.txt", "r") as f:
+        data = f.read().splitlines()
+        assert part1(data) == 0
+        assert part2(data) == 0
+        f.close()
+
+
+with open("day.txt", "r") as f:
     data = f.read().splitlines()
 
     part1(data)
 
     part2(data)
+
+    test_passing()
 
     f.close()
