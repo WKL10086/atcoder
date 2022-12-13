0f43a84fea57384ba865119b7de4ae11295c3692
Peter Wong <peter.wong@deepcodify.com>
2022-12-13 12:58:03 +0800

    Update template

--- adventofcode/template/day.py
+++ adventofcode/template/day.py
@@ -1,14 +1,12 @@
 def part1(data: list[str]) -> int:
     ans = 0
 
-    print(ans)
     return ans
 
 
 def part2(data: list[str]) -> int:
     ans = 0
 
-    print(ans)
     return ans
 
 
@@ -16,17 +14,17 @@ def test_passing():
     with open("test.txt", "r") as f:
         data = f.read().splitlines()
         assert part1(data) == 0
-        assert part2(data) == 0
+        # assert part2(data) == 0
         f.close()
 
 
 with open("day.txt", "r") as f:
     data = f.read().splitlines()
 
-    part1(data)
+    # part1(data)
 
     # part2(data)
 
-    # test_passing()
+    test_passing()
 
     f.close()
